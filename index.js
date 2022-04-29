ab5b12fba4a4501f15d09f9455b3f40696afb645
Fabizocker456 <fabian.schiener@gmx.de>
2022-04-29 07:22:12 +0200

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+import fs from 'fs';
+import yml from "js-yaml";
+import chalk from "chalk";
+import run from "./run.js";
+console.log(chalk.blue("setup..."))
+let loc = process.argv.length>2?process.argv[2]:"main.yml"
+const script = fs.readFileSync(loc,'utf8');
+var file = yml.load(script);
+let cur;
+console.log(chalk.blue("running..."))
+for(let i=0;i<file.length;i++){
+  cur=file[i];
+  run.run(cur);
+}
---
+++ run.js
@@ -0,0 +1,57 @@
+import fs from "fs"
+
+var funcs = {
+"print":a=>{console.log(a.a.join(''));return a.a.join('')},
+"raw":a=>(a.a[0]),
+"math":a=>{
+    let op = a.a[0]
+    let args=a.a.slice(1)
+    /**/ if(op=="sum"){return args[0] + args[1]}
+    else if(op=="sub"){return args[0] - args[1]}
+    else if(op=="mul"){return args[0] * args[1]}
+    else if(op=="div"){return args[0] / args[1]}
+
+    else if(op=="mod"){return args[0] % args[1]}
+    else if(op=="exp"){return args[0] ** args[1]}
+
+    else if(op=="min"){return Math.min(...args)}
+    else if(op=="max"){return Math.max(...args)}
+
+    else if(op=="inv"){return 1 / args[0]}
+    else if(op=="neg"){return 0 - args[0]}
+
+    else if(op=="pi"){return Math.PI}
+    else if(op=="e"){return Math.E}
+
+    else if(op=="sin"){return Math.sin(args[0]/180*Math.PI)}
+    else if(op=="cos"){return Math.cos(args[0]/180*Math.PI)}
+    else if(op=="tan"){return Math.tan(args[0]/180*Math.PI)}
+  },
+"get":a=>(Object.keys(a.w).includes(a.a[0]) ? a.w[a.a[0]] : null),
+"set":a=>{a.w[a.a[0]]=a.a[1];return a.a[1]},
+"read":a=>(fs.readFileSync(a.a.join(""))),
+"write":a=>(fs.writeFileSync(a.a[0],a.a.slice(1).join("")))
+}
+
+var world = {}
+
+function run(code){
+  if(!Object.keys(code).includes("action")){return code}
+  if(Object.keys(funcs).includes(code["action"])){
+    for(let i=0;i<code["args"].length;i++){
+      if(Object.keys(code["args"][i]).includes("action")){
+        code["args"][i]=run(code["args"][i])
+      }
+    }
+    return funcs[code["action"]]({a:code["args"],w:world})
+  } else if (["if","while","for","switch"].includes(code["action"])){      if(code["action"]=="if"){
+      if(run(code["cond"])){
+        for(let j=0;j<code["if"].length;j++){run(code["if"][j])}
+      }else{if(Object.keys(code).includes("else")){for(let j=0;j<code["else"].length;j++){run(code["else"][j])}}}
+    }
+  }
+}
+
+export default {
+    "run":run
+}
